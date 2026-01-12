a072c5c9fe24d804e46a93763ce36f2ad3ed5344
Fanju <98313711+Fanju6@users.noreply.github.com>
2026-01-12 20:35:38 +0800

    更新订阅UA

--- src/webui/src/ui/settings-page.ts
+++ src/webui/src/ui/settings-page.ts
@@ -6,7 +6,6 @@ import { setTheme } from 'mdui/functions/setTheme.js';
 import { UI } from './ui-core.js';
 import Sortable from 'sortablejs';
 
-// import logoUrl from '../assets/img/logo.png';
 const logoUrl = '/logo.png';
 
 interface RoutingRule {
